3fbaa8ab2fa88937ef1c11256af77a42fac12705
Crihalix <Crihalix@gmail.com>
2017-07-21 12:00:44 +0300

    react-routing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function About() {
+function About(props) {
     return (
         <main id="about">
-            <h1 className="mdc-typography--display1">О проекте</h1>
+            <h1 className="mdc-typography--display1">{props.title}</h1>
             <p className="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet nostrum blanditiis omnis ipsam voluptate aliquid qui, labore voluptas. Quae est quia, sapiente dolorum dolore debitis animi nobis autem atque? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis eum eveniet provident ut dicta asperiores voluptatem esse sed adipisci saepe optio placeat, id libero, ducimus at ratione eos quidem ab?</p>
             <p className="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi alias officiis porro. Facere, odio nulla modi veniam consequatur! Unde, ipsam, accusantium. Accusamus ipsam soluta velit dolor animi incidunt doloremque quis?</p>
         </main>
     );
+}
+
+About.propTypes = {
+    title: PropTypes.string.isRequired
 }
 
 export default About;
